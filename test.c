763b3b25bc8e50d0607ae558337343a914ba36c6
Sheezma yousuf <88012846+Sheezma@users.noreply.github.com>
2021-10-05 17:37:28 +0530

    Add files via upload

--- test.c
+++ test.c
@@ -1,37 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
-#include "doubly_linked_list.h"
-void print_list(DoubleNode *head) {
-for (DoubleNode* p = head; p != NULL; p = p->next)
-printf("%d ", p->value);
-printf("\n");
-}
+#include "circular_linked_list.h"
 int main(void) {
-DoubleNode *head = NULL;
-
-for (int i = 0; i < 10; i++)
-head = dll_prepend(head, i);
- print_list(head);
-assert(dll_find(head, 11) == NULL);
-DoubleNode* value_5 = dll_find(head, 5);
-dll_insert_before(value_5, 11);
-dll_insert_after(value_5, 12);
- print_list(head);
-  printf("Deleting 5\n");
-  head = dll_delete_node(head, value_5);
-  print_list(head);
-  assert(dll_find(head, 9) != NULL);                                                                                               
-  DoubleNode *value_nine = dll_find(head, 9);                                                                                      
-  printf("Deleting 9 \n");
-  head = dll_delete_node(head, value_nine);                                                                                                        
-  print_list(head);  
-  assert(dll_find(head, 8) != NULL);
-  DoubleNode *value_eight = dll_find(head, 8);                                                                                     
-  printf("Deleting 8 \n");
-  head = dll_delete_node(head, value_eight);
-  print_list(head);  
- 
-  return 0;
+Node *head = NULL;
+for (int i = 0; i < 10; i++) {
+head = cll_insert(head, i);
+  }
+  cll_print(head);
+  Node *any = cll_find(head, 5);
+  assert(any->data == 5);
+  head = cll_delete(any);
+cll_print(head);
+cll_insert_after(head, 100);
+cll_print(head);
+return 0;
 }
 
+
+  
